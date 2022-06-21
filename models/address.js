3bee90af26e6755822ae8bf96c8612b3fae85c43
Jacky <jacky@Jackys-MBP.fios-router.home>
2022-06-21 11:12:31 -0400

    adds googleId information and authorization

--- models/address.js
+++ models/address.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const addressSchema = new mongoose.Schema({
     name:String,
     address:String,
+    googleId: String
 
-})
+}, {timestamps: true})
 
 const Address = mongoose.model('Address', addressSchema);
 
